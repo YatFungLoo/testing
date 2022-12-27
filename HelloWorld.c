d6aa69c4cc0746be57195541d9392c60ca681489
Yat Fung <116527622+YatFungLoo@users.noreply.github.com>
2022-12-27 05:22:33 +0000

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
 
 void main(){
-<<<<<<< HEAD
-  printf("Whatttt?\n");
-=======
-  printf("Welcomeee, to the WORLDDD of TOMORROW!!!!!\n");
-  printf("changes by someone...\n");
->>>>>>> origin/newfeature
+  printf("I love Pizza!");
 }
